0062f6bdc7ea07be402e39856f7971b1a09ff09b
EliA437 <allenmwnc.eli@gmail.com>
2025-09-11 19:50:10 -0500

    Remove uneeded import.

--- frontend/pages/TopSongs.tsx
+++ frontend/pages/TopSongs.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 import TypingText from "../components/TypingText";
 import TerminalButton from "../components/TerminalButton";
 import WhiteScrollList from "../components/WhiteScrollList";
